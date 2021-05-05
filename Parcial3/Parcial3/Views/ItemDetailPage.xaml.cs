83d87ed2443fb83281aef616efdaf187800d66cf
Paul Bernal <82968269+PauloC21@users.noreply.github.com>
2021-05-05 11:06:31 -0600

    Creacion de app
    v1

---
+++ Parcial3/Parcial3/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Parcial3.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Parcial3.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
